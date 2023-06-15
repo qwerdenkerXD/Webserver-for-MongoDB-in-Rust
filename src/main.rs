a55645e457d3c259c734ffb07832dfd18f978235
Franz-Eric Sill <107982913+qwerdenkerXD@users.noreply.github.com>
2023-06-15 12:34:13 +0200

    Simple server with get, post, delete with actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
-use web_server;
-use std::panic::catch_unwind;
+use actix_web::{get, post, delete, web, App, HttpResponse, HttpServer};
+// use serde::{Deserialize, Serialize};
 
-fn main() {
-    let port = 1234;
-    connect(port)
+#[actix_web::main]
+async fn main() {
+    match start_server_on(3000).await {
+        Ok(_) => (),
+        Err(err) => println!("{}", err)
+    }
 }
 
-fn connect(port: i32) {
-    web_server::new()
-        .get("/", Box::new(|request: web_server::Request, mut response: web_server::Response|{
-            "Hello World!".into()
-        }))
-        .launch(port);
+#[get("/")]
+async fn hello() -> HttpResponse {
+    HttpResponse::Ok().body("Hello")
 }
+
+#[post("/")]
+async fn keks() -> HttpResponse {
+    HttpResponse::Ok().json("{\"test\":1}")
+}
+
+#[delete("/")]
+async fn bla() -> HttpResponse {
+    HttpResponse::Ok().body("Deleted")
+}
+
+async fn start_server_on(port: i32) -> Result<(), std::io::Error> {
+    let server = HttpServer::new(|| 
+        App::new().service(hello)
+                  .service(keks)
+                  .service(bla)
+                  )
+                  .bind(format!("127.0.0.1:{port}"))?;
+    println!("Listening on port {port}");
+    server.run().await
+}  
+
 // curl -d '{"rivers":[{"id":12,"length":12,"name":"Wasserhahn"}], "riverFlow":[{"river_id":12,"country_id":1}]}' -H "Content-Type: application/json" http://localhost:3000/
